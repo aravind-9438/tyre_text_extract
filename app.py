eab3f7acd624709bcd45f2b20fc3f51d914c3313
aravind reddy <vindara987@gmail.com>
2021-11-20 20:01:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ if(img_file is not None):
 		result_text = [] #empty list for results
 		
 		for text in result:
-			if(text[-1]>0.7):
+			if(text[-1]>0.6):
 				result_text.append(text[1])
 	st.write(result_text)
 	
