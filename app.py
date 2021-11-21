850d8d7b8de3e821c637790b888460b9bab4908e
aravind reddy <vindara987@gmail.com>
2021-11-21 07:36:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ reader = load_model()
 
 if(img_file is not None):
 	img = load_image(img_file)
-	img = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 	max_val=255
 	th = 125
